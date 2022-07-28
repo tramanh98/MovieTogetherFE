84c5fc1af82a0fc7918c245afd9c406560384554
Ngo Dien <ngodien.contact@gmail.com>
2022-07-28 18:08:36 +0700

    add video

--- video-app-fe/src/pages/home-page/home.tsx
+++ video-app-fe/src/pages/home-page/home.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export const HomePage = () => {
-  return <h1>Home Page!!!!</h1>;
+  const [video, setVideo] = useState<any>({});
+  useEffect(() => {
+    // try {
+    //   const response = async () => {
+    //     const { data } = await axios.get("http://localhost:3001/api/video");
+    //     setVideo([...data]);
+    //   };
+    //   response();
+    //   // const data = await response.json();
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  }, []);
+  return (
+    <div className="App">
+      <video controls muted autoPlay>
+        <source src="http://localhost:3001/api/video" type="video/mp4"></source>
+      </video>
+    </div>
+  );
 };
